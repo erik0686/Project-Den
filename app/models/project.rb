eda5dfe4e66cd1453faecc7b21d9c93d5733d3e6
Erik Torres <erik.torres.aguilar@gmail.com>
2017-12-01 19:22:04 -0600

    d

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,4 +4,9 @@ class Project < ApplicationRecord
 	has_many :project_users
 	has_many :users, through: :project_users
 	belongs_to :user
+
+
+	validates :max_capacity, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+	validates :name, :max_capacity, :description, :status, :start_date, :end_date, :profile, presence: true
+
 end
